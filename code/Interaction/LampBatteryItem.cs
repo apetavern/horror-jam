116b9d53af98e1ba7a59aa671f67ec7cbb744767
Matt <78914427+Matt9440@users.noreply.github.com>
2022-10-21 22:24:37 +0100

    Simple battery implementation

---
+++ code/Interaction/LampBatteryItem.cs
@@ -0,0 +1,29 @@
+﻿namespace GvarJam.Interactions;
+
+/// <summary>
+/// Represents an item that takes time to use.
+/// </summary>
+[Category( "Interactables" )]
+public partial class LampBatteryItem : DelayedUseItem
+{
+	public override float TimeToUse => 1;
+	protected override bool DeleteOnUse => true;
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		SetModel( "models/sbox_props/watermelon/watermelon.vmdl" );
+		SetupPhysicsFromModel( PhysicsMotionType.Static );
+	}
+
+	protected override void OnUsed( Entity user )
+	{
+		if ( user is not Pawn pawn )
+			return;
+
+		pawn.InsertNewLampBattery();
+
+		Log.Info( $"Battery level replenished for {user.Name}" );
+	}
+}
