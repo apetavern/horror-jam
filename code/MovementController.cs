10a4cf781e582b67d6306d598c3c7478165f1da8
Alex <alex@gu3.me>
2022-10-21 20:31:18 +0100

    Prevent players from just holding / spamming sprint

--- code/MovementController.cs
+++ code/MovementController.cs
@@ -1,11 +1,14 @@
 ﻿namespace GvarJam;
 
-public class MovementController : WalkController
+public partial class MovementController : WalkController
 {
 	private TimeSince TimeSinceSprinted;
 
 	// 0 to 1
-	public float Stamina { get; private set; }
+	[Net, Predicted] public float Stamina { get; set; }
+
+	// Is the player currently sprinting?
+	[Net, Predicted] public bool IsSprinting { get; set; }
 
 	// Per second, while sprinting
 	private float StaminaReductionRate => 0.5f;
@@ -15,22 +18,46 @@ public class MovementController : WalkController
 	// Seconds
 	private float StaminaReplenishDelay => 1.0f;
 
+	// How much stamina is required to engage sprint?
+	public float MinimumStaminaForSprint => 0.3f;
+
 	public override void Simulate()
 	{
 		base.Simulate();
 
-		if ( WishVelocity.Length > DefaultSpeed + 10f )
+		//
+		// Player started sprint?
+		//
+		if ( Input.Pressed( InputButton.Run ) && Stamina > MinimumStaminaForSprint )
 		{
+			IsSprinting = true;
+		}
+
+		//
+		// End sprint?
+		//
+		if ( Input.Released( InputButton.Run ) || Stamina <= 0f )
+		{
+			IsSprinting = false;
+		}
+
+		if ( IsSprinting )
+		{
+			//
+			// Sprint reduction
+			//
 			Stamina -= StaminaReductionRate * Time.Delta;
 			TimeSinceSprinted = 0;
 		}
-		else if ( TimeSinceSprinted > StaminaReplenishDelay )
+		else if ( !IsSprinting && TimeSinceSprinted > StaminaReplenishDelay )
 		{
+			//
+			// Sprint regen
+			//
 			Stamina += StaminaReplenishRate * Time.Delta;
 		}
 
 		Stamina = Stamina.Clamp( 0, 1 );
-
 		DebugOverlay.ScreenText( $"{Stamina}", 5 );
 	}
 
@@ -42,7 +69,7 @@ public class MovementController : WalkController
 		var ws = Duck.GetWishSpeed();
 		if ( ws >= 0 ) return ws;
 
-		if ( Input.Down( InputButton.Run ) && Stamina > 0 ) return SprintSpeed;
+		if ( Input.Down( InputButton.Run ) && IsSprinting ) return SprintSpeed;
 		if ( Input.Down( InputButton.Walk ) ) return WalkSpeed;
 
 		return DefaultSpeed;
