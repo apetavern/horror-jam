0237d2c9536b8975cb507f675432466d8a66a00c
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2022-10-21 16:43:20 -0300

    Add sound to lamp on/off

--- code/Pawn.Lamp.cs
+++ code/Pawn.Lamp.cs
@@ -29,7 +29,14 @@ public partial class Pawn
 		{
 			Lamp.Enabled = value;
 			if ( !value )
+			{
 				TimeSinceLampOff = 0;
+				Sound.FromEntity( "flashlight_click_off", this );
+			}
+			else
+			{
+				Sound.FromEntity( "flashlight_click_on", this );
+			}
 		}
 	}
 
