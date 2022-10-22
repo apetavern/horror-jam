a08ea95c018c5141414cc203516261d89d1cb305
Alex <alex@gu3.me>
2022-10-22 22:11:59 +0100

    Add player footstep sounds

--- code/Pawn.cs
+++ code/Pawn.cs
@@ -150,4 +150,43 @@ public sealed partial class Pawn : AnimatedEntity
 
 		return 1.0f;
 	}
+
+	TimeSince timeSinceLastFootstep = 0;
+
+	/// <summary>
+	/// A footstep has arrived!
+	/// </summary>
+	public override void OnAnimEventFootstep( Vector3 pos, int foot, float volume )
+	{
+		if ( LifeState != LifeState.Alive )
+			return;
+
+		if ( !IsClient )
+			return;
+
+		if ( timeSinceLastFootstep < 0.2f )
+			return;
+
+		volume *= FootstepVolume();
+
+		timeSinceLastFootstep = 0;
+
+		var tr = Trace.Ray( pos, pos + Vector3.Down * 20 )
+			.Radius( 1 )
+			.Ignore( this )
+			.Run();
+
+		if ( !tr.Hit ) return;
+
+		tr.Surface.DoFootstep( this, tr, foot, volume );
+	}
+
+	/// <summary>
+	/// Allows override of footstep sound volume.
+	/// </summary>
+	/// <returns>The new footstep volume, where 1 is full volume.</returns>
+	public float FootstepVolume()
+	{
+		return Velocity.WithZ( 0 ).Length.LerpInverse( 0.0f, 200.0f );
+	}
 }
