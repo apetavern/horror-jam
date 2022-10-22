112e6efa643f51f81851a32240e1ab4596b76478
Alex <alex@gu3.me>
2022-10-22 03:21:39 +0100

    Update CircleEx

--- code/Utility/GraphicsExtensions.cs
+++ code/Utility/GraphicsExtensions.cs
@@ -4,7 +4,7 @@ namespace GvarJam.Utility;
 
 public static class GraphicsExtensions
 {
-	public static void CircleEx( in Vector2 center, float outer, float inner, Color color, int points = 32, float startAngle = 0f, float endAngle = 360f, float uv = 0f )
+	public static void CircleEx( in Vector2 center, float outer, float inner, Color color, int points = 32, float startAngle = 0f, float endAngle = 360f )
 	{
 		var vertices = new List<Vertex>();
 
@@ -20,13 +20,13 @@ public static class GraphicsExtensions
 		{
 		}
 
-		float num2 = (endAngle - startAngle) % (twoPi + 0.01f);
-		if ( num2 <= 0f )
+		float size = (endAngle - startAngle) % (twoPi + 0.01f);
+		if ( size <= 0f )
 		{
 			return;
 		}
 
-		float frac = twoPi / (float)points;
+		float frac = twoPi / points;
 
 		for ( float i = startAngle; i < endAngle; i += frac )
 		{
@@ -47,13 +47,6 @@ public static class GraphicsExtensions
 			Vector2 uv3 = y / 2f + 0.5;
 			Vector2 uv4 = x * inner / outer / 2f + 0.5;
 			Vector2 uv5 = y * inner / outer / 2f + 0.5;
-			if ( uv > 0f )
-			{
-				uv2 = new Vector2( (angle - MathF.PI - startAngle) * uv / twoPi, 0f );
-				uv3 = new Vector2( (angleFrac - MathF.PI - startAngle) * uv / twoPi, 0f );
-				uv4 = uv2.WithY( 1f );
-				uv5 = uv3.WithY( 1f );
-			}
 
 			Vector2 v = center + x * outer;
 			AddVertex( v, color, uv2 );
