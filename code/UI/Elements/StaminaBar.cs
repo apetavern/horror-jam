aa849a62c177aaacfb6a01b1417bcfdba83e5dc3
Alex <alex@gu3.me>
2022-10-22 01:53:03 +0100

    Add label to stamina bar, use flex for layout

--- code/UI/Elements/StaminaBar.cs
+++ code/UI/Elements/StaminaBar.cs
@@ -6,8 +6,17 @@ internal class StaminaBar : Panel
 
 	public StaminaBar()
 	{
-		_ = Add.Icon( "bolt", "icon" );
-		FillPanel = Add.Panel( "fill" );
+		//
+		// Might move this to html?
+		//
+		var left = Add.Panel( "left" );
+		_ = left.Add.Icon( "bolt", "icon" );
+
+		var right = Add.Panel( "right" );
+		var bar = right.Add.Panel( "bar" );
+		FillPanel = bar.Add.Panel( "fill" );
+
+		_ = right.Add.Label( "Stamina", "label" );
 	}
 
 	public override void Tick()
@@ -17,7 +26,7 @@ internal class StaminaBar : Panel
 		if ( Local.Pawn is not Pawn { Controller: MovementController controller } )
 			return;
 
-		FillPanel.Style.Width = Box.Rect.Width * controller.Stamina * ScaleFromScreen;
+		FillPanel.Style.Width = Length.Fraction( controller.Stamina );
 		SetClass( "visible", !controller.Stamina.AlmostEqual( 1 ) );
 	}
 }
