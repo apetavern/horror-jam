664e2fc2c851df5385ba7dd576d15dffd30346a8
Alex <alex@gu3.me>
2022-10-23 17:36:20 +0100

    Split objective OnInteract into WithEntity and WithType

--- code/Objectives/Conditions/ObjectiveEndCondition.cs
+++ code/Objectives/Conditions/ObjectiveEndCondition.cs
@@ -4,7 +4,8 @@ public struct ObjectiveEndCondition
 {
 	public enum ConditionType
 	{
-		OnInteract,
+		OnInteractWithEntity,
+		OnInteractWithType,
 		OnTrigger,
 		OnTimer
 	}
@@ -14,8 +15,11 @@ public struct ObjectiveEndCondition
 	[ShowIf( nameof( Type ), ConditionType.OnTrigger )]
 	public string TriggerName { get; set; }
 
-	[ShowIf( nameof( Type ), ConditionType.OnInteract )]
+	[ShowIf( nameof( Type ), ConditionType.OnInteractWithEntity )]
 	public string InteractableName { get; set; }
+
+	[ShowIf( nameof( Type ), ConditionType.OnInteractWithType )]
+	public string TypeName { get; set; }
 
 	[ShowIf( nameof( Type ), ConditionType.OnTimer )]
 	public float TimeDelay { get; set; }
@@ -24,11 +28,18 @@ public struct ObjectiveEndCondition
 	{
 		switch ( Type )
 		{
-			case ConditionType.OnInteract:
+			case ConditionType.OnInteractWithEntity:
 				{
 					var entity = Entity.FindByName( InteractableName );
 
 					if ( pawn.IsInteracting && pawn.InteractedEntity == entity )
+						return true;
+
+					return false;
+				}
+			case ConditionType.OnInteractWithType:
+				{
+					if ( pawn.IsInteracting && pawn.InteractedEntity?.GetType().Name == TypeName )
 						return true;
 
 					return false;
