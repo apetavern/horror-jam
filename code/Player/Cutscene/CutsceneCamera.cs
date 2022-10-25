9b05256808287dc5650305105e0df5e1a1b6d584
Matt <78914427+Matt9440@users.noreply.github.com>
2022-10-25 20:04:51 +0100

    Fixed null errors

--- code/Player/Cutscene/CutsceneCamera.cs
+++ code/Player/Cutscene/CutsceneCamera.cs
@@ -3,14 +3,14 @@
 public partial class CutsceneCamera : CameraMode
 {
 	[Net]
-	public AnimatedEntity TargetEntity { get; set; }
+	public AnimatedEntity? TargetEntity { get; set; }
 
 	[Net]
-	public string TargetAttachment { get; set; }
+	public string? TargetAttachment { get; set; }
 
 	public override void Update()
 	{
-		var transform = TargetEntity.GetAttachment( TargetAttachment ) ?? default;
+		var transform = TargetEntity?.GetAttachment( TargetAttachment ) ?? default;
 
 		Position = transform.Position;
 		Rotation = transform.Rotation;
