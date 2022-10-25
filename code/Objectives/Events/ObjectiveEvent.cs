21e80e73b5296341cd9b78fd16c91e2c474aa830
Alex <alex@gu3.me>
2022-10-25 01:10:31 +0100

    Play cutscenes thru objective events, move cutscene code over to its own file

--- code/Objectives/Events/ObjectiveEvent.cs
+++ code/Objectives/Events/ObjectiveEvent.cs
@@ -5,6 +5,7 @@ public struct ObjectiveEvent
 	public enum EventType
 	{
 		PlaySound,
+		PlayCutscene
 	}
 
 	public EventType Type { get; set; }
@@ -12,12 +13,25 @@ public struct ObjectiveEvent
 	[ShowIf( nameof( Type ), EventType.PlaySound ), ResourceType( "sound" )]
 	public string SoundName { get; set; }
 
+	[ShowIf( nameof( Type ), EventType.PlayCutscene )]
+	public string TargetEntityName { get; set; }
+
+	[ShowIf( nameof( Type ), EventType.PlayCutscene )]
+	public string TargetEntityAttachment { get; set; }
+
+	[ShowIf( nameof( Type ), EventType.PlayCutscene )]
+	public float Duration { get; set; }
+
 	public void Invoke( Pawn pawn )
 	{
 		switch ( Type )
 		{
 			case EventType.PlaySound:
 				Sound.FromScreen( SoundName );
+				break;
+			case EventType.PlayCutscene:
+				var targetEntity = Entity.FindByName( TargetEntityName ) as ModelEntity;
+				pawn.StartCutscene( targetEntity, TargetEntityAttachment, Duration );
 				break;
 		}
 	}
