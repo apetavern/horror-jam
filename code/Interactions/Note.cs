7356f510a02c06207fe97d8153fd3ed5a65f6c70
Matt <78914427+Matt9440@users.noreply.github.com>
2022-10-27 00:39:58 +0100

    Add note entity

---
+++ code/Interactions/Note.cs
@@ -0,0 +1,22 @@
+﻿namespace GvarJam.Interactions;
+
+/// <summary>
+/// Represents an item that takes time to use.
+/// </summary>
+[Library( "ent_note" )]
+[HammerEntity]
+[EditorModel( "models/items/battery/battery.vmdl" )]
+
+public partial class Note : InstantUseItem
+{
+	[Net, Property]
+	public string NoteContents { get; set; }
+
+	protected override void OnUsed( Entity user )
+	{
+		base.OnUsed( user );
+
+		Log.Info( NoteContents );
+	}
+}
+
