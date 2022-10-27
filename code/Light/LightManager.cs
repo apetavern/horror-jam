1b51b014298243e94cb0dc462281a810fe229749
Matt <78914427+Matt9440@users.noreply.github.com>
2022-10-27 21:06:18 +0100

    Toggle lighting

---
+++ code/Light/LightManager.cs
@@ -0,0 +1,69 @@
+﻿namespace GvarJam.SoundManager;
+
+/// <summary>
+/// The light manager for the game.
+/// </summary>
+public static class LightManager
+{
+	static bool areLightsOn { get; set; }
+
+	static bool shouldLightsFlicker { get; set; }
+
+	static float timeBetweenFlicker { get; set; } = 1;
+
+	static TimeSince timeSinceLastFlicker { get; set; }
+
+	static void SetLightState( bool shouldBeOn )
+	{
+		// Dirty hack to change material group of all light models.
+		var allLightModelEnts = Entity.All.OfType<ModelEntity>();
+
+		foreach( var model in allLightModelEnts )
+			model?.SetMaterialGroup( shouldBeOn ? 0 : 1 );
+
+		var allLights = Entity.All.OfType<PointLightEntity>();
+
+		foreach ( var light in allLights )
+			light.Enabled = shouldBeOn;
+	}
+
+	/// <summary>
+	/// Turn the lighting on / off from a server sided environment.
+	/// </summary>
+	/// <param name="shouldBeOn"></param>
+	[ConCmd.Server]
+	public static void ToggleLighting( bool shouldBeOn )
+	{
+		SetLightState( shouldBeOn );
+	}
+
+	/// <summary>
+	/// Call in a server sided environment.
+	/// </summary>
+	/// <param name="shouldFlicker">Enable or disable the flicker.</param>
+	/// <param name="timeBetweenFlick">How fast the light should flash on and off.</param>
+	public static void FlickerLighting( bool shouldFlicker, float timeBetweenFlick )
+	{
+		shouldLightsFlicker = shouldFlicker;
+		timeBetweenFlicker = timeBetweenFlick;
+
+		if( !shouldFlicker )
+		{
+			SetLightState( true );
+		}
+	}
+
+	[Event.Tick.Server]
+	public static void Tick()
+	{
+		if ( shouldLightsFlicker && timeSinceLastFlicker > timeBetweenFlicker )
+		{
+			var allLights = Entity.All.OfType<PointLightEntity>();
+
+			foreach ( var light in allLights )
+				light.Enabled = !light.Enabled;
+
+			timeSinceLastFlicker = 0;
+		}
+	}
+}
