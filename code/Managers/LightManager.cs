b1300fce6db213554b2867bc77f249cbd1609833
Matt <78914427+Matt9440@users.noreply.github.com>
2022-10-28 23:34:21 +0100

    On by default

--- code/Managers/LightManager.cs
+++ code/Managers/LightManager.cs
@@ -5,7 +5,7 @@
 /// </summary>
 public static class LightManager
 {
-	static bool areLightsOn { get; set; }
+	static bool areLightsOn { get; set; } = true;
 
 	static bool shouldLightsFlicker { get; set; }
 
