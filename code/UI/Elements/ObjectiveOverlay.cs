abb5d1fbadab98f2478ab45d267601f36b812bc7
Alex <alex@gu3.me>
2022-10-29 14:33:34 +0100

    Buff objective overlay, displays notes

--- code/UI/Elements/ObjectiveOverlay.cs
+++ code/UI/Elements/ObjectiveOverlay.cs
@@ -5,9 +5,49 @@ public class ObjectiveOverlay : Panel
 	private const float Size = 32f;
 	private const float MaxDistance = 512f;
 
+	private Color ObjectiveColor => new Color( 0xFFFFFFFF );
+	private Color NoteColor => new Color( 0xFFFFE900 );
+
+	private float Margin = 64f;
+
+	private void DrawIcon( string icon, Entity entity, Color? _color = null )
+	{
+		const float inchesToMetres = 0.0254f;
+
+		var position = entity.WorldSpaceBounds.Center;
+		var distance = CurrentView.Position.Distance( position );
+		var alpha = distance.LerpInverse( MaxDistance, 128f );
+
+		var size = Size;
+
+		var screenPos = ((Vector2)position.ToScreen()) * Screen.Size;
+		screenPos = screenPos.Clamp( Margin, Screen.Size - Margin );
+		screenPos -= size / 2.0f;
+
+		var rect = new Rect( screenPos, size );
+
+		var color = _color ?? Color.White;
+		color = color.WithAlpha( alpha );
+
+		Graphics.DrawIcon( rect, icon, color, size );
+
+		rect.Position += new Vector2( 0, 32 );
+		rect.Position -= new Vector2( 64, 0 );
+		rect.Width = 160f;
+
+		string displayDistance = (distance * inchesToMetres).CeilToInt().ToString();
+		Graphics.DrawText( rect, $"{displayDistance}m", color, "Chakra Petch", 16, 400, TextFlag.Center );
+	}
+
 	public override void DrawBackground( ref RenderState state )
 	{
 		base.DrawBackground( ref state );
+		DrawObjectives();
+		DrawNotes();
+	}
+
+	private void DrawObjectives()
+	{
 
 		foreach ( var objective in Objectives.Instance.DisplayObjectives )
 		{
@@ -46,20 +86,17 @@ public class ObjectiveOverlay : Panel
 				if ( !entity.IsValid() || entity == null )
 					continue;
 
-				var position = entity.WorldSpaceBounds.Center;
-				var distance = CurrentView.Position.Distance( position );
-				var alpha = distance.LerpInverse( MaxDistance, 128f );
-
-				var size = Size;
-				var screenPos = position.ToScreen( Screen.Size ) ?? new Vector2( -128f );
-				screenPos -= size / 2.0f;
-
-				var rect = new Rect( screenPos, size );
-				var icon = "radio_button_checked";
-				var color = Color.White.WithAlpha( alpha );
-
-				Graphics.DrawIcon( rect, icon, color, size );
+				DrawIcon( "radio_button_checked", entity, ObjectiveColor );
 			}
+		}
+	}
+
+	private void DrawNotes()
+	{
+		foreach ( var note in Entity.All.OfType<Note>()
+			.Where( x => x.WorldSpaceBounds.Center.Distance( CurrentView.Position ) < MaxDistance ) )
+		{
+			DrawIcon( "description", note, NoteColor );
 		}
 	}
 }
