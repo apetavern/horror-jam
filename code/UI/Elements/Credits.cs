14fc7e7c78f5f890602c5c60baee2ec2f370d025
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2022-10-30 10:39:54 -0300

    Add RPC for displaying credits

--- code/UI/Elements/Credits.cs
+++ code/UI/Elements/Credits.cs
@@ -3,29 +3,22 @@
 [UseTemplate]
 public sealed class Credits : Panel
 {
+	public static Credits Instance { get; private set; }
+
 	public Label GameOutcomeLabel { get; set; }
 	public Label ThanksLabel { get; set; }
 	public Label CreditsLabel { get; set; }
 
 	public Credits()
 	{
+		Instance = this;
+
 		AddClass( "hide" );
 	}
 
-	public override void Tick()
+	[ClientRpc]
+	public static void DisplayCredits()
 	{
-		if ( ObjectiveSystem.Current is null )
-			return;
-
-		if ( ObjectiveSystem.Current.ActiveObjectives.Count == 0 
-			&& ObjectiveSystem.Current.PendingObjectives.Count == 0 )
-		{
-			RemoveClass( "hide" );
-		} 
-		else
-		{
-			AddClass( "hide" );
-		}
+		Instance.RemoveClass( "hide" );
 	}
-
 }
